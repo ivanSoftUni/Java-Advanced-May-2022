a01c00a14ffbbba6b80e793d0492b0dcf7a8a31f
ivanSoftUni <ivan_pz86@mail.bg>
2022-06-03 13:08:29 +0300

    Exercises

---
+++ MultidimensionalArraysExercises/_03DiagonalDifference.java
@@ -0,0 +1,13 @@
+package MultidimensionalArraysExercises;
+
+import java.util.Scanner;
+
+public class _03DiagonalDifference {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+
+    }
+}
