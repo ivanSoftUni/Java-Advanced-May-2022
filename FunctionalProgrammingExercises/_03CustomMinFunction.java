b3d3fe6129ccca868ef90717b6bed024697542f2
ivanSoftUni <ivan_pz86@mail.bg>
2022-06-10 23:45:03 +0300

    Update Problem

--- FunctionalProgrammingExercises/_03CustomMinFunction.java
+++ FunctionalProgrammingExercises/_03CustomMinFunction.java
@@ -1,12 +1,34 @@
 package FunctionalProgrammingExercises;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.function.Function;
 
 public class _03CustomMinFunction {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        int[] numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt).toArray();
+        Function<int[], Integer> function = (int[] arr) -> {
+            int min = Integer.MAX_VALUE;
+            for (int element : arr) {
+                if (element < min) {
+                    min = element;
+                }
+            }
+            return min;
+        };
 
+        System.out.println(function.apply(numbers));
+    }
 
+    public static Integer main(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        for (int element : arr) {
+            if (element < min) {
+                min = element;
+            }
+        }
+        return min;
     }
 }
