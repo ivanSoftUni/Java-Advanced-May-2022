9197b659b5f3fde0005739032d743122e757204b
ivanSoftUni <ivan_pz86@mail.bg>
2022-06-11 00:16:35 +0300

    Exercises

---
+++ FunctionalProgrammingExercises/_06PredicateForNames.java
@@ -0,0 +1,13 @@
+package FunctionalProgrammingExercises;
+
+import java.util.Scanner;
+
+public class _06PredicateForNames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+
+}
