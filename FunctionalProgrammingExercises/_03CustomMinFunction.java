baf059806cf40394beaa2a1ccfbab4d2de44e899
ivanSoftUni <ivan_pz86@mail.bg>
2022-06-10 21:56:13 +0300

    Exercises

---
+++ FunctionalProgrammingExercises/_03CustomMinFunction.java
@@ -0,0 +1,12 @@
+package FunctionalProgrammingExercises;
+
+import java.util.Scanner;
+
+public class _03CustomMinFunction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+
+
+    }
+}
