bad1fd542189c87ef5208261f16bb02c6e85c0a0
ivanSoftUni <ivan_pz86@mail.bg>
2022-06-11 00:16:20 +0300

    Exercises

--- FunctionalProgrammingExercises/_05ReverseAndExclude.java
+++ FunctionalProgrammingExercises/_05ReverseAndExclude.java
@@ -1,5 +1,20 @@
 package FunctionalProgrammingExercises;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class _05ReverseAndExclude {
-    
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toList());
+        int n = Integer.parseInt(scanner.nextLine());
+        Collections.reverse(numbers);
+        numbers.stream()
+                .filter(e -> e % n != 0)
+                .forEach(e -> System.out.print(e + " "));
+
+    }
 }
