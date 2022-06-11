43eed02f9dfb4da1245f4499b48fadf5bbed1841
ivanSoftUni <ivan_pz86@mail.bg>
2022-06-11 16:26:56 +0300

    Exercise

--- FunctionalProgrammingExercises/_06PredicateForNames.java
+++ FunctionalProgrammingExercises/_06PredicateForNames.java
@@ -1,12 +1,19 @@
 package FunctionalProgrammingExercises;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class _06PredicateForNames {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int maxLength = Integer.parseInt(scanner.nextLine());
 
+        List<String> names = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .collect(Collectors.toList());
 
+        names.stream().filter(e -> e.length() <= maxLength).forEach(System.out::println);
 
     }
 
