088c015e89b9980f281ee5960b03fa4b5d36ac3a
ivanSoftUni <ivan_pz86@mail.bg>
2022-06-14 15:53:04 +0300

    Lab

---
+++ DefiningClasses/_01CarInfo/Car.java
@@ -0,0 +1,36 @@
+package _01CarInfo;
+
+public class Car {
+    public String brand;
+    public String model;
+    public int horsePower;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String carInfo() {
+        return String.format("The car is: %s %s - %d HP.", getBrand(), getModel(), getHorsePower());
+    }
+
+}
